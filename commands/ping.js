479ab8ddadb516fac1255f7ffdf9c8eafb603ad3
Blake <49827919+Plysm@users.noreply.github.com>
2020-06-20 16:08:26 -0400

    Fixed naming typo

--- commands/ping.js
+++ commands/ping.js
@@ -1,12 +1,13 @@
 const Discord = require('discord.js');
 const { executionAsyncResource } = require('async_hooks');
+const name = global.name;
 
 module.exports = {
     name: 'ping',
     description: 'pong!',
     execute(message, args){
         const embed = new Discord.MessageEmbed()
-        .setTitle('Valios - Ping')
+        .setTitle(`${name} - Ping`)
         .setDescription('Pong!')
         .setColor(0x28D69F)
         .setThumbnail('https://i.imgur.com/8kK5AKY.png')
